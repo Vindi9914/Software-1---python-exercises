ef9b1608e8cda69aa045e1713a48a36c45715d80
Vindi9914 <ruvindi9914@gmail.com>
2026-08-25 19:22:47 +0300

    add module 03 solutions

---
+++ module 01&02/myfile.py
@@ -0,0 +1,23 @@
+print ('Hello World!')
+print ("Hello World!")
+
+print ('"Hello" said Joe')
+
+print ("Good")  
+print ("Morning")
+
+print ("Good\nMorning")
+
+
+user = input('Ruvindi Rashmika: ')
+print ("Nice to meet you, " + user + "!")
+
+
+color = input ("Blue")
+
+
+points = 50
+print (points)
+
+points = 120
+print (points)
---
+++ module 01&02/readme.md
@@ -0,0 +1,4 @@
+# Software 1 - Python Exercises
+**Ruvindi Rashmika**
+## Module 1
+I Completed exercise 1 and 2
